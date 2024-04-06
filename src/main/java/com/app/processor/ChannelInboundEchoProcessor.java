0a1a633af64b3509b13c3d16d7e918bb8aeed15b
duyenthaind <thaind@stringee.com>
2024-04-06 17:20:15 +0700

    feature - comment echo processor

--- src/main/java/com/app/processor/ChannelInboundEchoProcessor.java
+++ src/main/java/com/app/processor/ChannelInboundEchoProcessor.java
@@ -9,16 +9,16 @@ import io.netty.channel.ChannelHandlerContext;
 public class ChannelInboundEchoProcessor implements ChannelInboundProcessor {
     @Override
     public void process(ChannelHandlerContext ctx, AppPacket packet) {
-        System.out.println("echoooooo");
-        if (packet.getData() != null && packet.getData().length > 0) {
-            String dataContent = new String(packet.getData());
-            System.out.println("This packet is " + dataContent);
-            if (dataContent.equals("test")) {
-                AppPacket resPacket = new AppPacket();
-                resPacket.setData("ok".getBytes());
-                resPacket.setLength("ok".length());
-                ctx.writeAndFlush(resPacket);
-            }
-        }
+//        System.out.println("echoooooo");
+//        if (packet.getData() != null && packet.getData().length > 0) {
+//            String dataContent = new String(packet.getData());
+//            System.out.println("This packet is " + dataContent);
+//            if (dataContent.equals("test")) {
+//                AppPacket resPacket = new AppPacket();
+//                resPacket.setData("ok".getBytes());
+//                resPacket.setLength("ok".length());
+//                ctx.writeAndFlush(resPacket);
+//            }
+//        }
     }
 }
